87a53dd55792b5a23ff548c3d857e5914c47aa05
emeraldng <ifglitterthen@gmail.com>
2018-08-09 14:35:38 -0500

    Actualizando rama Esme

--- capture_photos/app.js
+++ capture_photos/app.js
@@ -1,23 +1,23 @@
-console.log("HOLA!");
+// console.log("HOLA!");
 
 
-(function(){
-    var video = document.getElementById('video'),
-    vendorUrl = window.URL || window.webkitURL;
+// (function(){
+//     var video = document.getElementById('video'),
+//     vendorUrl = window.URL || window.webkitURL;
 
-    navigator.getMedia = (navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia ||
-                         navigator.msGetUserMedia);
+//     navigator.getMedia = (navigator.getUserMedia ||
+//                          navigator.webkitGetUserMedia ||
+//                          navigator.mozGetUserMedia ||
+//                          navigator.msGetUserMedia);
                         
-   navigator.getMedia ({
-       video: true,
-       audio: false
-   }, function(stream){
-       video.src = window.URL.createObjectURL(stream);
-       video.play();
-   }, function (error){
-       console.log("error!");
+//    navigator.getMedia ({
+//        video: true,
+//        audio: false
+//    }, function(stream){
+//        video.src = window.URL.createObjectURL(stream);
+//        video.play();
+//    }, function (error){
+//        console.log("error!");
        
-   });                     
-}) ();
+//    });                     
+// }) ();
