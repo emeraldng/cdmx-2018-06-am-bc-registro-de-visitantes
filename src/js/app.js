3974f56abe6bea55e0993c5a4186259fb48a8a05
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-08-08 20:22:47 -0500

    subiendo intento de botón signin

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,14 @@
 // declarando botones
 let register = document.getElementById('button-registro');
+let signInHerf = document.getElementById('sign-in-herf');
 
 const beginRegister = () => {
   location.href = '../src/views/view1.html';
 };
 register.addEventListener('click', beginRegister);
+
+const signInPage = () =>{
+  // location.href = '../src/views/view2.html';
+  console.log('holi');
+};
+signInHerf.addEventListener('click', signInPage);
