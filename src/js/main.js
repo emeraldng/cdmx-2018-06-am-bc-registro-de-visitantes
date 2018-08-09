81f72a8accc3385fe91e11d43e7bfa978b4cb521
emeraldng <ifglitterthen@gmail.com>
2018-08-09 16:54:56 -0500

    Declarando los valores que tomará las ramas de FireBase

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,51 @@
+import { domainToASCII } from "url";
+
+console.log("hola");
+
 let db = firebase.firestore(); // Variable que inicializa Firestore
 
 let completeNameInput = document.getElementById('complete-name');
 let emailInput = document.getElementById('email');
 let companyInput = document.getElementById('company');
+let asuntoInput = document.getElementById('asunto');
 let sendBtn = document.getElementById('send-form-btn');
+
 
 sendBtn.addEventListener('click', () => {
   if (companyInput.value === '' || emailInput.value === ' ' || companyInput.value === '') {
     alert('Es necesario llenar todos los campos');
   } else {
+    let completeNameValue= document.getElementById('complete-name').value;
+    let emailValue = document.getElementById('email').value;
+    let companyValue = document.getElementById('company').value;
+    let asuntoValue = document.getElementById('asunto').value;
     db.collection('visitors').add({
-      visitor: completeNameInput.value,
-      email: emailInput.value,
-      company: companyInput.value,
+      visitor: completeNameValue,
+      email: emailValue,
+      company: companyValue,
       foto: '',
-      asunto: '',
-      time: ''
+      asunto: asuntoValue,
+      time: Date(asuntoValue)
     })
       .then(function(docRef) {
         console.log('Document written with ID: ', docRef.id);
+      })
+      .catch(function(error) {
+        console.error('Error adding document: ', error);
       });
   };
 });
 
+//funcion 
+
+
+
+
+
+
+
+
+//Funciones para la camara y foto.
 var player = document.getElementById('player'); 
 var snapshotCanvas = document.getElementById('snapshot');
 var captureButton = document.getElementById('capture');
