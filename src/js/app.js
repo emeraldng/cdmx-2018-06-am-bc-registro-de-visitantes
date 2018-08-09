ce855d1ef98f8d7a513f48632e81270ec58763ef
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-08-09 08:10:58 -0500

    añadiendo html de sign in y reasignando nombres de los archivos

--- src/js/app.js
+++ src/js/app.js
@@ -3,12 +3,6 @@ let register = document.getElementById('button-registro');
 let signInHerf = document.getElementById('sign-in-herf');
 
 const beginRegister = () => {
-  location.href = '../src/views/view1.html';
+  location.href = '../src/views/form.html';
 };
 register.addEventListener('click', beginRegister);
-
-const signInPage = () =>{
-  // location.href = '../src/views/view2.html';
-  console.log('holi');
-};
-signInHerf.addEventListener('click', signInPage);
