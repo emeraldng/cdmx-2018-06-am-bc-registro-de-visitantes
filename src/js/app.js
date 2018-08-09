77d44ed4cc09fc8e21459038783f96aa443d3c38
DianaCarrillo <39560661+DianaCarrillo@users.noreply.github.com>
2018-08-08 19:25:55 -0500

    subiendo cambios para guardar en la base de datos

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,7 @@
 // declarando botones
-let register = document.getElementById('button-registro'); 
-
+let register = document.getElementById('button-registro');
 
 const beginRegister = () => {
-//   console.log('holi');
   location.href = '../src/views/view1.html';
 };
 register.addEventListener('click', beginRegister);
