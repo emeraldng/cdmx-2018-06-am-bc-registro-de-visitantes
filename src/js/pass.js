b5dfa91bfe0f5e229527a4b757da4ab70bed1f11
emeraldng <ifglitterthen@gmail.com>
2018-08-14 09:29:42 -0500

    Readme e investigación de Alejandra (ux)

--- src/js/pass.js
+++ src/js/pass.js
@@ -1,17 +1,17 @@
-$('#button').click(function() {
-  var doc = new jsPDF();
+// $('#button').click(function() {
+//   var doc = new jsPDF();
             
 
-  var name = $('#fullname').val();
-  var company = $('#email').val();
-  var department = $('#company').val();
-  var title = $('#title').val();
+//   var name = $('#fullname').val();
+//   var company = $('#email').val();
+//   var department = $('#company').val();
+//   var title = $('#title').val();
 
-  doc.setFontSize(26);
-  doc.setTextColor(92, 76, 76);
+//   doc.setFontSize(26);
+//   doc.setTextColor(92, 76, 76);
 
-  doc.text(23, 81, name);
-  doc.text(23, 122, company);
-  doc.text(23, 162, department);
-  // doc.text(23, 202, title);
-  doc.save('CredencialVisitante.pdf');
+//   doc.text(23, 81, name);
+//   doc.text(23, 122, company);
+//   doc.text(23, 162, department);
+//   // doc.text(23, 202, title);
+//   doc.save('CredencialVisitante.pdf');
